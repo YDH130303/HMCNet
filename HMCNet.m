30cc0a6ce422e87df9815c53405bb1a8c0e6b3a1
YDH130303 <63883081+YDH130303@users.noreply.github.com>
2025-11-06 09:03:18 +0800

    Update HMCNet.m

--- HMCNet.m
+++ HMCNet.m
@@ -1,1 +1,2 @@
-Remove HMCNet.m content (under embargo)
+The code implementation for this paper is currently under embargo.
+We are committed to the principles of reproducibility and open science. The complete, well-documented source code will be made publicly available in this repository upon the paper's acceptance.
